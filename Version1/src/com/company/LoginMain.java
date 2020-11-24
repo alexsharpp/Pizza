aeef52f420bc1a7c2acb9cdbc56636573c496cc6
Tristan RJ <rutherfordjamest@lordwandsworth.org>
2020-11-24 14:46:33 +0000

    commit test tris 2

--- Version1/src/com/company/LoginMain.java
+++ Version1/src/com/company/LoginMain.java
@@ -1,8 +1,10 @@
+package com.company;
 
-/*
-public class
+public class Main {
+    static void signUp
 
-public void signUp{
+    {
+        System.out.println("signUp method executed");
     /*
     ask for user name
     ask for password
@@ -13,8 +15,12 @@ public void signUp{
     -studentLoginFile
     return to main
      */
-        }
-public void login{
+    }
+
+    static void login
+
+    {
+        System.out.println("login method executed");
     /*
     login function
     who login as - teacher or student
@@ -23,13 +29,15 @@ public void login{
         check with appropiate file
         if -
      */
-        }
+    }
 
-public static void main(String[]args){
+
+    public static void main(String[] args) {
+        System.out.println("Main login start");
         /*
         sign up or log in
         if sign up - go to create account
         if log in - go to login
-         */ /*
-        }
          */
+    }
+}
